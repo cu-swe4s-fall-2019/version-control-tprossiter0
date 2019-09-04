55cde37d8fb1e5e55fe3f6ba8a86d2150dc628f0
root <root@Unbound.localdomain>
2019-09-04 13:37:38 -0600

    fixed ordering

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 chmod -x run.sh
+conda activate swe4s
+echo ...running calculate.py...
+
 python3 calculate.py --intadd1 5 --intadd2 5 --numerator 10 --denominator 2
+echo ...end of run...
